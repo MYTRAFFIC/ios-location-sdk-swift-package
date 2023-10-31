67620e1dd45207171b89ac508c5b2a6e13c63d1f
Sébastien Diemer <sdiemer@mytraffic.fr>
2023-10-31 12:30:29 +0100

    publishing version v3.2.1

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MyTrafficLocation",
-            url: "https://s3.eu-central-1.amazonaws.com/public-artifacts.mytraffic.io/location-sdk-ios/v3.2.0/MyTrafficLocation.xcframework.zip",
-            checksum: "bed96755058588a463adc4eb3d64f162424e5b407efff5d32ca486aa4b7c95c4"
+            url: "https://s3.eu-central-1.amazonaws.com/public-artifacts.mytraffic.io/location-sdk-ios/v3.2.1/MyTrafficLocation.xcframework.zip",
+            checksum: "f377cecaf5bddc01fec19302bb3a83f25f5331ed40dd9119ef4a07b3524b5480"
         ),
     ]
 )
